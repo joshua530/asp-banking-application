2223003e3433c4c91c707fd04e491e761704fe5a
joshua530 <joshuaomari5@gmail.com>
2022-03-31 11:49:38 +0300

    add Equals method

--- MvcBankingApplication/Utils/StockObj.cs
+++ MvcBankingApplication/Utils/StockObj.cs
@@ -1,6 +1,6 @@
 namespace MvcBankingApplication.Utils
 {
-    class StockObj
+    public class StockObj
     {
         public string Symbol { get; set; } = String.Empty;
         public string Price { get; set; } = String.Empty;
@@ -9,6 +9,20 @@ namespace MvcBankingApplication.Utils
         public override string ToString()
         {
             return $"{Symbol} | {Price} | {PercentChange}";
+        }
+
+        public override bool Equals(Object? compare)
+        {
+            if (compare == null || !this.GetType().Equals(compare.GetType()))
+                return false;
+            StockObj tmp = (StockObj)compare;
+            if (tmp.Symbol != Symbol)
+                return false;
+            if (tmp.Price != Price)
+                return false;
+            if (tmp.PercentChange != PercentChange)
+                return false;
+            return true;
         }
     }
 }
