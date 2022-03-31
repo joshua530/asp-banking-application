364bfa6c58c15092532d3c56f5367508e317a539
joshua530 <joshuaomari5@gmail.com>
2022-03-31 12:33:28 +0300

    add stock fetching method to StockApiModel

--- MvcBankingApplication/Models/StockApiModel.cs
+++ MvcBankingApplication/Models/StockApiModel.cs
@@ -1,7 +1,16 @@
+using MvcBankingApplication.Services.Stocks;
+using MvcBankingApplication.Utils;
+
 namespace MvcBankingApplication.Models.StockApiModel
 {
+
     public class StockApiModel
     {
+        public StockApiService StockApi = new StockApiService();
 
+        public StockObj[] GetStocks()
+        {
+            return StockApi.Stocks;
+        }
     }
 }
