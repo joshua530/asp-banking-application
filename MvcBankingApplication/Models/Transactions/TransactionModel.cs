f83c7a72ca939894e8ac8445b590e8b824ffe113
joshua530 <joshuaomari5@gmail.com>
2022-04-01 14:16:35 +0300

    add formatters and validators for transaction model

--- MvcBankingApplication/Models/Transactions/TransactionModel.cs
+++ MvcBankingApplication/Models/Transactions/TransactionModel.cs
@@ -1,15 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MvcBankingApplication.Models.Transactions
 {
     public class TransactionModel
     {
         public int Id { get; }
+        [Required]
+        [DataType(DataType.DateTime), DisplayFormat(
+            DataFormatString = "H:mm:ss dd-MM-yyyy"
+        )]
         public DateTime TimeOfTransaction { get; set; }
+        [Required]
         public int CustomerId { get; set; }
         public int? CashierId { get; set; }
         public int? ApproverId { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
         public double Amount { get; set; }
+        [Required]
         public int AccountDebitedId { get; set; }
+        [Required]
         public int AccountCreditedId { get; set; }
+        [Required]
         public String TransactionType { get; set; } = String.Empty;
     }
 }
