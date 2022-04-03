8f5b31a3e0d1733091fade37f98bf4f85155fc6a
joshua530 <joshuaomari5@gmail.com>
2022-04-03 09:47:02 +0300

    add primary key and account number

--- MvcBankingApplication/Models/Accounts/AccountModel.cs
+++ MvcBankingApplication/Models/Accounts/AccountModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MvcBankingApplication.Models.Accounts
 {
     abstract public class AccountModel
     {
-        public int Id { get; }
+        public int ID { get; set; }
+        [Required]
+        public int AccountNumber { get; set; }
         public string Type { get; set; } = String.Empty;
         public double Balance { get; set; } = 0;
     }
