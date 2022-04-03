5efca2219c27e5528b64a9082e8b160e575230d5
joshua530 <joshuaomari5@gmail.com>
2022-04-03 14:26:06 +0300

    use primitive for TransactionType

--- MvcBankingApplication/Models/Transactions/TransactionModel.cs
+++ MvcBankingApplication/Models/Transactions/TransactionModel.cs
@@ -2,27 +2,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MvcBankingApplication.Models.Transactions
 {
-    public class TransactionModel
-    {
-        public int Id { get; }
-        [Required]
-        [DataType(DataType.DateTime), DisplayFormat(
-            DataFormatString = "H:mm:ss dd-MM-yyyy"
-        )]
-        public DateTime TimeOfTransaction { get; set; }
-        [Required]
-        public int CustomerId { get; set; }
-        public int? CashierId { get; set; }
-        public int? ApproverId { get; set; }
-        [Required]
-        [DataType(DataType.Currency)]
-        public double Amount { get; set; }
-        [Required]
-        public int AccountDebitedId { get; set; }
-        [Required]
-        public int AccountCreditedId { get; set; }
-        [Required]
-        [EnumDataType(typeof(TransactionTypes))]
-        public object TransactionType { get; set; }
-    }
+  public class TransactionModel
+  {
+    public int Id { get; }
+    [Required]
+    [DataType(DataType.DateTime), DisplayFormat(
+        DataFormatString = "H:mm:ss dd-MM-yyyy"
+    )]
+    public DateTime TimeOfTransaction { get; set; }
+    [Required]
+    public int CustomerId { get; set; }
+    public int? CashierId { get; set; }
+    public int? ApproverId { get; set; }
+    [Required]
+    [DataType(DataType.Currency)]
+    public double Amount { get; set; }
+    [Required]
+    public int AccountDebitedId { get; set; }
+    [Required]
+    public int AccountCreditedId { get; set; }
+    [Required]
+    [EnumDataType(typeof(TransactionTypes))]
+    public string TransactionType { get; set; }
+  }
 }
