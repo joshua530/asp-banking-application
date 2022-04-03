ae279dcd8f1c3e2618c012fc45212ab181befa5d
joshua530 <joshuaomari5@gmail.com>
2022-04-03 21:47:45 +0300

    add NotificationModel to application context

--- MvcBankingApplication/Data/ApplicationContext.cs
+++ MvcBankingApplication/Data/ApplicationContext.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MvcBankingApplication.Models.Users;
 using MvcBankingApplication.Models.Transactions;
+using MvcBankingApplication.Models.Notifications;
 
     public class ApplicationContext : DbContext
     {
@@ -21,4 +22,6 @@ using MvcBankingApplication.Models.Transactions;
         public DbSet<MvcBankingApplication.Models.Users.CashierModel> CashierModel { get; set; }
 
         public DbSet<MvcBankingApplication.Models.Users.AdminModel> AdminModel { get; set; }
+
+        public DbSet<MvcBankingApplication.Models.Notifications.NotificationModel> NotificationModel { get; set; }
     }
