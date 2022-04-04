530c8641a3b7f7b4eeb63164fc3710fc2819cf43
joshua530 <joshuaomari5@gmail.com>
2022-04-04 10:11:28 +0300

    add accounts to application context

--- MvcBankingApplication/Data/ApplicationContext.cs
+++ MvcBankingApplication/Data/ApplicationContext.cs
@@ -8,20 +8,26 @@ using MvcBankingApplication.Models.Users;
 using MvcBankingApplication.Models.Transactions;
 using MvcBankingApplication.Models.Notifications;
 
-    public class ApplicationContext : DbContext
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
     {
-        public ApplicationContext (DbContextOptions<ApplicationContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<MvcBankingApplication.Models.Users.CustomerModel> CustomerModel { get; set; }
-
-        public DbSet<MvcBankingApplication.Models.Transactions.TransactionModel> TransactionModel { get; set; }
-
-        public DbSet<MvcBankingApplication.Models.Users.CashierModel> CashierModel { get; set; }
-
-        public DbSet<MvcBankingApplication.Models.Users.AdminModel> AdminModel { get; set; }
-
-        public DbSet<MvcBankingApplication.Models.Notifications.NotificationModel> NotificationModel { get; set; }
     }
+
+    public DbSet<MvcBankingApplication.Models.Users.CustomerModel> CustomerModel { get; set; }
+
+    public DbSet<MvcBankingApplication.Models.Transactions.TransactionModel> TransactionModel { get; set; }
+
+    public DbSet<MvcBankingApplication.Models.Users.CashierModel> CashierModel { get; set; }
+
+    public DbSet<MvcBankingApplication.Models.Users.AdminModel> AdminModel { get; set; }
+
+    public DbSet<MvcBankingApplication.Models.Notifications.NotificationModel> NotificationModel { get; set; }
+
+    public DbSet<MvcBankingApplication.Models.Accounts.CustomerAccountModel> CustomerAccountModel { get; set; }
+
+    public DbSet<MvcBankingApplication.Models.Accounts.BankCashAccountModel> BankCashAccountModel { get; set; }
+
+    public DbSet<MvcBankingApplication.Models.Accounts.BankOverdraftAccountModel> BankOverdraftAccountModel { get; set; }
+}
