0996dc94ae7668476e79be8bc00d95a2a2c979d5
joshua530 <joshuaomari5@gmail.com>
2022-04-04 10:12:52 +0300

    delete todo comment

--- MvcBankingApplication/Models/Accounts/CustomerAccountModel.cs
+++ MvcBankingApplication/Models/Accounts/CustomerAccountModel.cs
@@ -3,6 +3,6 @@ namespace MvcBankingApplication.Models.Accounts
     public class CustomerAccountModel : AccountModel
     {
         public double OverdraftLimit { get; set; } = 0;
-        public int UserId { get; set; } //TODO FK
+        public int UserId { get; set; }
     }
 }
