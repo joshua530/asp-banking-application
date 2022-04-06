75b6c4f022003ff6070a87cf3bd40ff1019354ad
joshua530 <joshuaomari5@gmail.com>
2022-04-06 12:02:21 +0300

    add UpdateImageUrl migration

---
+++ MvcBankingApplication/Migrations/20220406082158_UpdateImageUrl.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcBankingApplication.Migrations
+{
+    public partial class UpdateImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                defaultValue: "/images/users/avatar.png",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true,
+                oldDefaultValue: "/images/users/avatar.png");
+        }
+    }
+}
