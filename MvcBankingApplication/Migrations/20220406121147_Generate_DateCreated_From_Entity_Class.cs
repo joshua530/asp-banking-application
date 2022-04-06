4e956fb72fbbddb687800c0c307482eed4f761c2
joshua530 <joshuaomari5@gmail.com>
2022-04-06 15:12:25 +0300

    generate DateCreated from ApplicationUser entity class

---
+++ MvcBankingApplication/Migrations/20220406121147_Generate_DateCreated_From_Entity_Class.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcBankingApplication.Migrations
+{
+    public partial class Generate_DateCreated_From_Entity_Class : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
