cd8350aff6d7d340aad47b37256903c30dc83045
joshua530 <joshuaomari5@gmail.com>
2022-04-06 11:10:17 +0300

    add AddImageUrl migration

---
+++ MvcBankingApplication/Migrations/20220406080946_AddImageUrl.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcBankingApplication.Migrations
+{
+    public partial class AddImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
