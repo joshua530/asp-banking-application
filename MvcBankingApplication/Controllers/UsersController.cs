0af2a6ce36c7b256d99d57d937345c7991bcb8b3
joshua530 <joshuaomari5@gmail.com>
2022-04-06 22:09:03 +0300

    add user controller

---
+++ MvcBankingApplication/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using MvcBankingApplication.Models.Users;
+using MvcBankingApplication.Models.ViewModels;
+
+
+namespace MvcBankingApplication.Controllers;
+
+#pragma warning disable CS8632
+public class UsersController : Controller
+{
+    public UserManager<ApplicationUser> _userManager;
+    public SignInManager<ApplicationUser> _signInManager;
+
+    public UsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    // GET: /Users/Profile/<id>
+    [Authorize]
+    public async Task<IActionResult> Profile(string? userId)
+    {
+        if (userId == null)
+        {
+            return View("NotFound404");
+        }
+
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user == null)
+        {
+            return View("NotFound404");
+        }
+
+        var profile = new Profile
+        {
+            FullName = user.FullName,
+            ImageUrl = user.ImageUrl,
+            DateCreated = user.DateCreated,
+            Email = user.Email,
+            UserName = user.UserName
+        };
+
+        var userObj = _userManager.GetUserAsync(User).GetAwaiter().GetResult();
+        if (userId == userObj.Id)
+        {
+            profile.IsProfileOwner = true;
+        }
+
+        return View(profile);
+    }
+}
+#pragma warning restore CS8632
