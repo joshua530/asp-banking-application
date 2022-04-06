102921263a2fc641f281fcece44209f1cfe8198f
joshua530 <joshuaomari5@gmail.com>
2022-04-06 12:42:29 +0300

    add date created field migration

--- MvcBankingApplication/Migrations/20220406094116_Add_User_DateCreatedField.Designer.cs
+++ MvcBankingApplication/Migrations/20220406094116_Add_User_DateCreatedField.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MvcBankingApplication.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220406094116_Add_User_DateCreatedField")]
+    partial class Add_User_DateCreatedField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.3");
