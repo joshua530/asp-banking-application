1e7838b338d24cfa2a50717955ed9c7cb52ebf3a
joshua530 <joshuaomari5@gmail.com>
2022-04-08 18:40:59 +0300

    delete unnecessary properties

--- MvcBankingApplication/Models/Users/BankStaff.cs
+++ MvcBankingApplication/Models/Users/BankStaff.cs
@@ -2,7 +2,5 @@ namespace MvcBankingApplication.Models.Users
 {
     abstract public class BankStaff : ApplicationUser
     {
-        public string StaffId { get; set; } = String.Empty;
-        public bool IsAdmin { get; set; } = false;
     }
 }
