a201f5266ada17dd5ca349f67e32b9bdf2e7172f
joshua530 <joshuaomari5@gmail.com>
2022-04-08 23:00:17 +0300

    update customer home view

--- MvcBankingApplication/Controllers/CustomersController.cs
+++ MvcBankingApplication/Controllers/CustomersController.cs
@@ -6,22 +6,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using MvcBankingApplication.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using MvcBankingApplication.Models.Users;
+using Microsoft.AspNetCore.Authorization;
+
 
 namespace MvcBankingApplication.Controllers
 {
     public class CustomersController : Controller
     {
         private readonly ApplicationContext _context;
+        UserManager<ApplicationUser> _userManager;
 
-        public CustomersController(ApplicationContext context)
+        public CustomersController(ApplicationContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        [Authorize(Roles = "customer")]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+            var homeModel = new CustomerHomeModel
+            {
+                Name = user.FullName
+            };
+            return View(homeModel);
         }
     }
 }
