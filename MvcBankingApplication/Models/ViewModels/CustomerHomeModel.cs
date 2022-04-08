b814b7d2a27d3c8d024566fe57219abe6a64b08a
joshua530 <joshuaomari5@gmail.com>
2022-04-08 22:57:42 +0300

    add customer home page model

---
+++ MvcBankingApplication/Models/ViewModels/CustomerHomeModel.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using MvcBankingApplication.Models.Users;
+
+namespace MvcBankingApplication.Models.ViewModels;
+
+public class CustomerHomeModel
+{
+    public string Name { get; set; }
+    // account
+    // account transactions -> separate model
+    // stock quotes
+}
