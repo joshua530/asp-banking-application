de3244393341fe28bfeb58a5d4d88ddb0b673a05
joshua530 <joshuaomari5@gmail.com>
2022-04-08 18:00:09 +0300

    delete boilerplate comment

--- MvcBankingApplication/Program.cs
+++ MvcBankingApplication/Program.cs
@@ -8,7 +8,6 @@ using MvcBankingApplication.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ApplicationContext");
-
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services
@@ -41,7 +40,6 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
