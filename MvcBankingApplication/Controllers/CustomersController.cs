2b5458398e0f6653eeeb0286a5015f6d623a646d
joshua530 <joshuaomari5@gmail.com>
2022-04-09 10:53:57 +0300

    update customer home page controller data

--- MvcBankingApplication/Controllers/CustomersController.cs
+++ MvcBankingApplication/Controllers/CustomersController.cs
@@ -9,7 +9,9 @@ using Microsoft.EntityFrameworkCore;
 using MvcBankingApplication.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using MvcBankingApplication.Models.Users;
+using MvcBankingApplication.Models.Accounts;
 using Microsoft.AspNetCore.Authorization;
+using MvcBankingApplication.Models;
 
 
 namespace MvcBankingApplication.Controllers
@@ -29,10 +31,19 @@ namespace MvcBankingApplication.Controllers
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
+            var customer = (Customer)user;
+            IQueryable<CustomerAccount> query = from c_a in _context.CustomerAccounts
+                                                where c_a.CustomerId == customer.Id
+                                                select c_a;
+            var stockApi = new StockApiModel();
+
             var homeModel = new CustomerHomeModel
             {
-                Name = user.FullName
+                User = customer,
+                Account = await query.FirstAsync(),
+                Stocks = stockApi.GetStocks()
             };
+
             return View(homeModel);
         }
     }
