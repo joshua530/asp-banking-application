e5b472c817392f35a0c4200488fb9ee930101bdb
joshua530 <joshuaomari5@gmail.com>
2022-04-09 10:58:05 +0300

    change indentation and correct expiration time generation method

--- MvcBankingApplication/Services/Stocks/StockApiService.cs
+++ MvcBankingApplication/Services/Stocks/StockApiService.cs
@@ -1,77 +1,76 @@
 using StackExchange.Redis;
+using MvcBankingApplication.Models;
 using MvcBankingApplication.Utils;
 using System.Globalization;
 using System.Xml.Serialization;
 
 namespace MvcBankingApplication.Services.Stocks
 {
-
-
-  public class StockApiService
-  {
-    public String[] StockNames = StockNamesService.Stocks;
-    IDatabase Cache = RedisConnectorHelper.Connection.GetDatabase();
-
-    public StockObj[] Stocks
+    public class StockApiService
     {
-      get
-      {
-        RedisValue cachedStocks = Cache.StringGet("stocks");
-        RedisValue expirationTime = Cache.StringGet("expiration_time");
-        StockObj[] stocks;
-        if (cachedStocks == RedisValue.Null || ExpirationTimePassed((string)expirationTime))
+        public String[] StockNames = StockNamesService.Stocks;
+        IDatabase Cache = RedisConnectorHelper.Connection.GetDatabase();
+
+        public StockModel[] Stocks
         {
-          stocks = ApiRequests.GetDataForStocks(StockNames);
-          // serialize the stocks
-          string serializedStocks = SerializeStocks(stocks);
-          Cache.StringSet("stocks", serializedStocks);
-          // set the timestamp at which the data will become stale
-          Cache.StringSet("expiration_time", CurrentTime());
-          return stocks;
+            get
+            {
+                RedisValue cachedStocks = Cache.StringGet("stocks");
+                RedisValue expirationTime = Cache.StringGet("expiration_time");
+                StockModel[] stocks;
+                if (cachedStocks == RedisValue.Null || ExpirationTimePassed((string)expirationTime))
+                {
+                    stocks = ApiRequests.GetDataForStocks(StockNames);
+                    // serialize the stocks
+                    string serializedStocks = SerializeStocks(stocks);
+                    Cache.StringSet("stocks", serializedStocks);
+                    // set the timestamp at which the data will become stale
+                    Cache.StringSet("expiration_time", GenerateExpirationTime(5));
+                    return stocks;
+                }
+                string stockStr = Cache.StringGet("stocks");
+                return DeserializeStocksStr(stockStr);
+            }
         }
-        string stockStr = Cache.StringGet("stocks");
-        return DeserializeStocksStr(stockStr);
-      }
-    }
 
-    public string SerializeStocks(StockObj[] stocks)
-    {
-      XmlSerializer serializer = new XmlSerializer(typeof(StockObj[]));
-      using (StringWriter sw = new StringWriter())
-      {
-        serializer.Serialize(sw, stocks);
-        return sw.ToString();
-      }
-    }
+        public string SerializeStocks(StockModel[] stocks)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(StockModel[]));
+            using (StringWriter sw = new StringWriter())
+            {
+                serializer.Serialize(sw, stocks);
+                return sw.ToString();
+            }
+        }
 
-    public StockObj[] DeserializeStocksStr(string stocksStr)
-    {
-      XmlSerializer serializer = new XmlSerializer(typeof(StockObj[]));
-      using (StringReader sr = new StringReader(stocksStr))
-      {
-        return (StockObj[])serializer.Deserialize(sr);
-      }
-    }
+        public StockModel[] DeserializeStocksStr(string stocksStr)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(StockModel[]));
+            using (StringReader sr = new StringReader(stocksStr))
+            {
+                return (StockModel[])serializer.Deserialize(sr);
+            }
+        }
 
-    private string CurrentTime()
-    {
-      return DateTime.Now.ToString("H:mm:ss dd-MM-yyyy");
-    }
+        private string GenerateExpirationTime(int numMinutes)
+        {
+            return DateTime.Now.AddMinutes(numMinutes).ToString("H:mm:ss dd-MM-yyyy");
+        }
 
-    public bool ExpirationTimePassed(string time)
-    {
-      if (time == RedisValue.Null) return true;
-      DateTime expirationTime = DateTime.ParseExact(time, "H:mm:ss dd-MM-yyyy", CultureInfo.InvariantCulture);
-      DateTime now = DateTime.Now;
+        public bool ExpirationTimePassed(string time)
+        {
+            if (time == RedisValue.Null) return true;
+            DateTime expirationTime = DateTime.ParseExact(time, "H:mm:ss dd-MM-yyyy", CultureInfo.InvariantCulture);
+            DateTime now = DateTime.Now;
 
-      if (expirationTime < now)
-      {
-        return true;
-      }
-      else
-      {
-        return false;
-      }
+            if (expirationTime < now)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
-  }
 }
