6bcdb306823cbfcf417a83ded4545d3f01b28096
joshua530 <joshuaomari5@gmail.com>
2022-04-09 10:55:39 +0300

    update customer home model
    properties added to home model are:
    	- User
    	- Account
     - Stocks
     - StockMarketLink

--- MvcBankingApplication/Models/ViewModels/CustomerHomeModel.cs
+++ MvcBankingApplication/Models/ViewModels/CustomerHomeModel.cs
@@ -1,12 +1,19 @@
-using Microsoft.AspNetCore.Identity;
+using MvcBankingApplication.Models.Accounts;
 using MvcBankingApplication.Models.Users;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcBankingApplication.Models.ViewModels;
 
 public class CustomerHomeModel
 {
-    public string Name { get; set; }
+    public Customer User { get; set; }
+    public CustomerAccount Account { get; set; }
     // account
     // account transactions -> separate model
     // stock quotes
+    public StockModel[] Stocks { get; set; }
+    public string StockMarketLink
+    {
+        get { return "https://www.nasdaq.com/market-activity/stocks"; }
+    }
 }
