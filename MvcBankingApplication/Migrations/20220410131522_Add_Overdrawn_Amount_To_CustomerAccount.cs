80e339c736daa9f310a70fddaa0f40a76f1149d8
joshua530 <joshuaomari5@gmail.com>
2022-04-10 18:17:45 +0300

    add OverdrawnAmount to CustomerAccount

---
+++ MvcBankingApplication/Migrations/20220410131522_Add_Overdrawn_Amount_To_CustomerAccount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcBankingApplication.Migrations
+{
+    public partial class Add_Overdrawn_Amount_To_CustomerAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "OverdrawnAmount",
+                table: "AccountModel",
+                type: "REAL",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OverdrawnAmount",
+                table: "AccountModel");
+        }
+    }
+}
