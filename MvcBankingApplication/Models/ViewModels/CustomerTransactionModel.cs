fa63802fefc73b79a46eaa84418b4db46459596a
joshua530 <joshuaomari5@gmail.com>
2022-04-15 16:30:41 +0300

    add CustomerTransactionModel

--- MvcBankingApplication/Models/ViewModels/CustomerTransactionModel.cs
+++ MvcBankingApplication/Models/ViewModels/CustomerTransactionModel.cs
@@ -3,12 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MvcBankingApplication.Models.ViewModels;
 
-public class CustomerTransactionModel
+public class CustomerTransactionModel : TransactionViewModel
 {
-    public string TimeString { get; set; }
-    public int AccountCreditedId { get; set; }
-    public int AccountDebitedId { get; set; }
-    public int Id { get; set; }
-    public double Amount { get; set; }
-    public string TransactionTypeStr { get; set; }
 }
