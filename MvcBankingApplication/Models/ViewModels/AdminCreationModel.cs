452ed526636d11291579d44934c81f06d792a9d0
joshua530 <joshuaomari5@gmail.com>
2022-04-16 07:51:26 +0300

    add model validation

--- MvcBankingApplication/Models/ViewModels/AdminCreationModel.cs
+++ MvcBankingApplication/Models/ViewModels/AdminCreationModel.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MvcBankingApplication.Models.ViewModels;
 
 public class AdminCreationModel
 {
+    [Required]
+    [MinLength(3), MaxLength(20)]
     public string Username { get; set; }
+    [Required]
+    [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
 }
